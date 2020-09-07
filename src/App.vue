7f68c6e4202e3eaf623867b11f004a7e78988298
Darrel Toledo <darreltoledo@icloud.com>
2020-09-07 23:07:37 +0100

    Footer added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,38 @@
   <v-app>
     <v-content>
       <router-view></router-view>
+      <v-footer
+        dark
+        padless
+      >
+      <v-card
+        class="flex"
+        flat
+        tile
+      >
+      <v-card-title class="blue-grey darken-3">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          dark
+          icon
+          :href="icon.href"
+          target="_blank"
+        >
+          <v-icon size="24px">{{ icon.icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} Groupomania — <strong>&#169; Darrel Toledo</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
     </v-content>
   </v-app>
 </template>
@@ -13,7 +45,24 @@ export default {
 
   },
   data: () => ({
-    //
+    icons: [
+      {
+        icon: 'mdi-facebook',
+        href: 'https://facebook.com'
+      },
+      {
+        icon: 'mdi-twitter',
+        href: 'https://twitter.com'
+      },
+      {
+        icon: 'mdi-linkedin',
+        href: 'https://linkedin.com'
+      },
+      {
+        icon: 'mdi-instagram',
+        href: 'https://instagram.com'
+      }
+    ]
   })
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <v-content with="400">
       <v-container>
         <v-card class="mx-auto mt-12">
+          <v-img class="logo-img" max-height="100" with="100" p src="../assets/logo.png"></v-img>
           <v-card-title class="mx-4">
             <h1 class="display-1">Login</h1>
           </v-card-title>
@@ -11,6 +12,7 @@
                 <v-text-field
                   label= "Username"
                   prepend-icon="mdi-account-circle"
+                  required
                 />
                 <v-text-field
                   :type="showPassword ? 'text' : 'password' "
@@ -18,14 +20,25 @@
                   prepend-icon="mdi-lock"
                   :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                   @click:append="showPassword = !showPassword"
+                  required
                 />
               </v-form>
             </v-card-text>
             <v-divider></v-divider>
             <v-card-actions>
-              <v-btn class="success">Register</v-btn>
+              <v-btn
+                class="success"
+                @click="register"
+              >
+              register
+              </v-btn>
                 <v-spacer></v-spacer>
-              <v-btn class="info">Sign in</v-btn>
+              <v-btn
+                class="info"
+                @click="signIn"
+                >
+                Sign in
+              </v-btn>
             </v-card-actions>
         </v-card>
       </v-container>
