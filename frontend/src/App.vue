e30dd7fffb6b8e2c6abb01a58b0c9410138a6bce
Darrel Toledo <darreltoledo@icloud.com>
2020-10-03 17:41:22 +0100

    App.vue script added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,15 @@
 <template>
   <v-app>
-    <router-view />
+    <router-view></router-view>
   </v-app>
 </template>
 
 <script>
 
+export default {
+  name: 'app',
+  data: () => ({
+    //
+  })
+}
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #ECEFF1;
-}
-
-#nav {
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
