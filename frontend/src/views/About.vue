76e03405ab4ae341a1b5a601fbb390c466411b48
Darrel Toledo <darreltoledo@icloud.com>
2021-02-22 10:51:22 +0000

    About background image removed

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-container>
+  <v-container class="about-container">
     <v-layout>
       <v-flex xs12>
         <h1 class="display-1 mt-5 px-2">Rules and regulations of Groupomania</h1>
         <p class="subheading font-weight-regular mt-5 px-2">
-          You can publish messages alone or accompanied with the choice of a gif or an image.
+          You can publish messages alone or accompanied with the choice of a GIF or an image.
             <br>
-            You can add a profile picture and fill in your bio to describe yourself by clicking on the colored avatar to access the account once logged in.
+            You can add a profile picture and fill in your bio to describe yourself by clicking on the coloured avatar to access the account once logged in.
             <br>
             Thank you for remaining kind and tolerant with others
              users. Any failure to comply with the regulations may result in
@@ -20,3 +20,10 @@
     </v-layout>
   </v-container>
 </template>
+
+<style lang="scss" scoped>
+// */.about-container {
+//  background-image: url("../assets/Presentation-Board.png");
+//  background-size: cover;
+//}
+</style>
