10d5404a94ff31d68e8ceb41e9ad7784b053fef0
Darrel Toledo <darreltoledo@icloud.com>
2021-02-28 14:55:04 +0000

    added comment on the comment model

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    //here it associate the model to another models
     static associate(models) {
       // define association here
       models.Comment.belongsTo(models.User, 
