176de5bfbbc1197ab9400f61e53b127fd4739ecf
Darrel Toledo <darreltoledo@icloud.com>
2021-09-23 21:47:07 +0100

    edit profile card

--- frontend/src/components/Account.vue
+++ frontend/src/components/Account.vue
@@ -8,7 +8,7 @@
               Go Back
             </v-btn>
             <v-card-title flat dense dark class="profile-title mr-3"
-              ><h1 class="title">My Profile</h1>
+              ><h1 class="title">Edit Profile</h1>
             </v-card-title>
             <div class="delete-account">
               <v-tooltip v-if="!$store.state.user.admin === true" bottom>
